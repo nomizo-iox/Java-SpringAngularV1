41c5b6801c2644a5ca5ee51db68661f3894ac929
nomizo-iox <nomizo.iox@gmail.com>
2020-03-19 21:35:18 -0500

    first commit

---
+++ src/test/java/com/angularcrud/AngularCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angularcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AngularCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
